0398aa0b65abad0263f266997f10a3a92fb8eaac
your_github_username <taalaibekuulusanzhar@gmail.com>
2022-02-11 11:26:09 +0600

    add a background image

--- src/main/java/com/kindson/fleetapp/controllers/ClientController.java
+++ src/main/java/com/kindson/fleetapp/controllers/ClientController.java
@@ -1,4 +1,13 @@
 package com.kindson.fleetapp.controllers;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ClientController {
+    @GetMapping("/")
+    public String getHome(){
+        return  "index";
+    }
 }
