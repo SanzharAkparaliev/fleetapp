8e569723278090e6196ef03edd4381a9816f51bb
your_github_username <taalaibekuulusanzhar@gmail.com>
2022-02-13 22:45:55 +0600

    display list of countries in the html page

--- src/main/java/com/kindson/fleetapp/controllers/CountryController.java
+++ src/main/java/com/kindson/fleetapp/controllers/CountryController.java
@@ -1,12 +1,24 @@
 package com.kindson.fleetapp.controllers;
 
+import com.kindson.fleetapp.models.Country;
+import com.kindson.fleetapp.services.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class CountryController {
+    @Autowired
+    CountryService countryService;
+
     @GetMapping("/countries")
-    public String getCountries(){
+    public String getCountries(Model model) {
+        List<Country> countryList = countryService.getCountries() ;
+        model.addAttribute("countries",countryList);
         return "country";
+
     }
 }
--- src/main/java/com/kindson/fleetapp/services/CountryService.java
+++ src/main/java/com/kindson/fleetapp/services/CountryService.java
@@ -1,8 +1,19 @@
 package com.kindson.fleetapp.services;
 
 
+import com.kindson.fleetapp.models.Country;
+import com.kindson.fleetapp.repositories.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CountryService {
+    @Autowired
+    CountryRepository countryRepository;
+
+    public List<Country> getCountries(){
+        return countryRepository.findAll();
+    }
 }
